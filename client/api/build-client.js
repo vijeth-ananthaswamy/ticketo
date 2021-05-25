1f748a4856146cd722b30bff4c94471b23a1181d
vijeth-ananthaswamy <33414284+vijeth-ananthaswamy@users.noreply.github.com>
2021-05-26 00:49:38 +1000

    Final config changes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL:'http://www.ticketo.club/',
+        //'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
